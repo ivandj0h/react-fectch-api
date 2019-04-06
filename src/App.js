1288986f8fdaf3127e2b849c593150a6e5e1cf46
ivandjoh <ivandi.djoh@gmail.com>
2019-04-06 17:16:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      return (
+        <div className="App">
+        <h3 className="list-group-item list-group-item-action active">Daftar User</h3>
+          <ul className="list-group">
+            {items.map(item => (
+              <li key={item.id} className="list-group-item list-group-item-action">
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+
 }
 
 export default App;
